8199f8deeceaef50784a4b2e19db368567330540
Zach Schuermann <zachary.zvs@gmail.com>
2021-04-01 00:07:30 -0500

    trying out wezterm

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm';
+
+return {
+    font_size = 13.0,
+    font = wezterm.font("Menlo"), 
+    dpi = 144.0,
+    font_antialias = "Greyscale", -- None, Greyscale, Subpixel
+    font_hinting = "Full", -- None, Vertical, VerticalSubpixel, Full
+}
